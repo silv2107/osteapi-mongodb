5a1168c2055b40eb0e0e782901705b23cb905f4c
Silviu Iosif <6332838@rts-365.dk>
2020-12-16 09:06:49 +0100

    cors added

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 var formidable = require("express-formidable");
+var cors = require("cors");
 
 require("./database");
+//origins der har adgang - default er alle
+app.use(cors());
 app.use(formidable());
 
 require("./cheeses.route")(app);
